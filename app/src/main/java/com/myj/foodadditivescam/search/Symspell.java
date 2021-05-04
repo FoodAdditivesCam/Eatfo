32512b06232f8dd1af53a094c4a4441904aa503e
beansbin <beans_bin@naver.com>
2021-05-04 12:20:24 +0900

    Modify Symspell

--- app/src/main/java/com/myj/foodadditivescam/search/Symspell.java
+++ app/src/main/java/com/myj/foodadditivescam/search/Symspell.java
@@ -1,6 +1,41 @@
 package com.myj.foodadditivescam.search;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Symspell {
+    public static String symspell(String input) {
+        String result= "";
 
+        try {
+            URL url = new URL("http://3.35.255.25:80/symspell/" + input);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                result += output;
+            }
 
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
+
