59a9b5167e927749f5c69a7c6ff8fa4af383f3a5
지인 <sonia9.shin@gmail.com>
2021-05-13 11:24:35 +0900

    move operation from show result to show info
    원래 show result에 있던 기능과 xml show info로 옮김

--- app/src/main/java/com/myj/foodadditivescam/OCR/ShowInfo.java
+++ app/src/main/java/com/myj/foodadditivescam/OCR/ShowInfo.java
@@ -1,8 +1,14 @@
 package com.myj.foodadditivescam.OCR;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.myj.foodadditivescam.R;
 
@@ -12,5 +18,24 @@ public class ShowInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_info);
+        TextView imageDetail = (TextView) findViewById(R.id.image_details);
+        imageDetail.setVisibility(View.GONE);
+
+        String[] res = getIntent().getStringArrayExtra("itemName");
+        Log.d(ShowResult.class.getSimpleName(), "인텐트 가져옴 "+res.length+"개");
+        RecyclerAdapter adapter = new RecyclerAdapter(res);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(this, ImageLoadActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
