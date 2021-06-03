def9158f62daaa0b2dc845d82bc2522fb1fe7033
지인 <sonia9.shin@gmail.com>
2021-06-03 16:20:06 +0900

    Create Splash Activity
    Splash Activity 생성

---
+++ app/src/main/java/com/myj/foodadditivescam/Splash.java
@@ -0,0 +1,15 @@
+package com.myj.foodadditivescam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
