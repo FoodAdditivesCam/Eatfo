3ac0686dda94d76e69c901b7586ccf1a7a47012e
지인 <sonia9.shin@gmail.com>
2021-06-03 16:25:45 +0900

    Update Splash.java
    스플래시 액티비티 생성
    이후 SharedPreferences 브랜치 머지 되면 업데이트 예정(**주석 참고)

--- app/src/main/java/com/myj/foodadditivescam/Splash.java
+++ app/src/main/java/com/myj/foodadditivescam/Splash.java
@@ -2,7 +2,11 @@ package com.myj.foodadditivescam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.myj.foodadditivescam.OCR.OCRMainActivity;
 
 public class Splash extends AppCompatActivity {
 
@@ -11,5 +15,16 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //여기에 sharedPreference 생성하고 최초 실행인지 판단해서
+                //어떤 액티비티로 넘길지 결정하는 코드 추가하는게 좋을 것 같음
+                Intent intent = new Intent(getApplicationContext(), OCRMainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
