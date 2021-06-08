6991d9d2def0a63cbcfdb0cf831f97f8a9903f2f
지인 <sonia9.shin@gmail.com>
2021-06-09 02:28:16 +0900

    Update wordCloud.java
    걍 처음부터 다시 만드는게 낫겠음 외부 라이브러리쓰려던거 다 지워버림

--- app/src/main/java/com/myj/foodadditivescam/wordCloud/wordCloud.java
+++ app/src/main/java/com/myj/foodadditivescam/wordCloud/wordCloud.java
@@ -7,59 +7,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class wordCloud {
-    /**
-     * The Text.
-     */
-    String text;
-    /**
-     * The Weight.
-     */
-    int weight;
+    public Bitmap createWordCloud(String data){
 
-    /**
-     * Instantiates a new WordCloud.
-     *
-     * @param text   the text
-     * @param weight the weight
-     */
-    public WordCloud(String text, int weight) {
-        this.text = text;
-        this.weight = weight;
-    }
-
-    /**
-     * Gets text.
-     *
-     * @return the text
-     */
-    public String getText() {
-        return text;
-    }
-
-    /**
-     * Sets text.
-     *
-     * @param text the text
-     */
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    /**
-     * Gets weight.
-     *
-     * @return the weight
-     */
-    public int getWeight() {
-        return weight;
-    }
-
-    /**
-     * Sets weight.
-     *
-     * @param weight the weight
-     */
-    public void setWeight(int weight) {
-        this.weight = weight;
+        return null;
     }
 }
