9a595893a5cd7bdcb05a3e4accced41553624b90
Minjeong Kim <alswjd980512@naver.com>
2021-06-08 14:23:58 +0900

    connect server db

--- app/src/main/java/com/myj/foodadditivescam/search/GetResult.java
+++ app/src/main/java/com/myj/foodadditivescam/search/GetResult.java
@@ -1,11 +1,7 @@
 package com.myj.foodadditivescam.search;
-
-
 import android.util.Log;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,18 +12,16 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-
 public class GetResult {
     public static JSONObject POST(JSONObject input) {
         JSONObject json = null;
         try{
-            URL url = new URL("http://3.35.255.25:80/result");
+            URL url = new URL("http://10.0.2.2/result"); // 3.35.255.25:80
             HttpURLConnection urlConn = (HttpURLConnection)url.openConnection();
             urlConn.setRequestMethod("POST");
             urlConn.setRequestProperty("Accept-Charset", "utf-8"); // Accept-Charset 설정.
             urlConn.setRequestProperty("Context_Type", "application/x-www-form-urlencoded");
             urlConn.setRequestProperty("Content-Type", "application/json");
-
             System.out.println(input);
             String str = input.toString();
             System.out.println(str);
@@ -35,25 +29,18 @@ public class GetResult {
             outputStream.write(str.getBytes("UTF-8"));
             outputStream.flush();
             outputStream.close();
-
             if(urlConn.getResponseCode() != HttpURLConnection.HTTP_OK){
                 Log.d("http_test", "getResponseFail");
                 return null;
             }
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), "UTF-8"));
-
             String line;
             String result = "";
-
             while((line = reader.readLine()) != null){
                 result += line;
             }
             System.out.println(result.toString());
-
             json = new JSONObject(result);
-
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -61,8 +48,6 @@ public class GetResult {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
         return json;
-
     }
 }
