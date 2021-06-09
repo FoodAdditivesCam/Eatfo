b5977a3802b984f40cdd39ae5b1b5aa278d33e40
지인 <sonia9.shin@gmail.com>
2021-06-09 11:58:25 +0900

    Update ShowInfo.java
    불필요한 import 삭제

--- app/src/main/java/com/myj/foodadditivescam/result/ShowInfo.java
+++ app/src/main/java/com/myj/foodadditivescam/result/ShowInfo.java
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.myj.foodadditivescam.OCR.RecyclerAdapter;
 import com.myj.foodadditivescam.R;
 
 public class ShowInfo extends AppCompatActivity {
