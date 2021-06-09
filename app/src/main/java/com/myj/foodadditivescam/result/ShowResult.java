4c046224023e28dcdb08e584d9a8a5f40decf400
beansbin <beans_bin@naver.com>
2021-06-09 15:46:56 +0900

    Add Glide gradle

--- app/src/main/java/com/myj/foodadditivescam/result/ShowResult.java
+++ app/src/main/java/com/myj/foodadditivescam/result/ShowResult.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.myj.foodadditivescam.OCR.ImageLoadActivity;
 import com.myj.foodadditivescam.R;
 import com.myj.foodadditivescam.RawMaterials;
@@ -31,9 +32,9 @@ public class ShowResult extends AppCompatActivity {
             tags+=rm.getTags()+" ";
         }
 //        wordCloud wcd = new wordCloud();
-//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 //        //태그 리스트 주고 워드클라우드 그려서 imageView 수정해주기
-//        imageView.setImageBitmap(wcd.createWordCloud(tags));
+        Glide.with(this).load(url).into(imageView);
 
         LinearLayout linearLayout = findViewById(R.id.linearLayout2);
         for (RawMaterials rm : rms) {
