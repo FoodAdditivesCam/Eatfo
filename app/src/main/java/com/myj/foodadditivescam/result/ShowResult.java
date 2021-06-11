6d587e5c7bc58f6718c53201fe7896cfb3795c91
Minjeong Kim <alswjd980512@naver.com>
2021-06-11 16:17:58 +0900

    add all_material button

--- app/src/main/java/com/myj/foodadditivescam/result/ShowResult.java
+++ app/src/main/java/com/myj/foodadditivescam/result/ShowResult.java
@@ -38,8 +38,26 @@ public class ShowResult extends AppCompatActivity {
         Glide.with(this).load(url).into(imageView);
 
         LinearLayout linearLayout = findViewById(R.id.linearLayout2);
+
+        // 전체 원재료 버튼 추가
+        Button tallBtn = new Button(this);
+        tallBtn.setId(-1);
+        tallBtn.setText("전체 원재료");
+        tallBtn.setHeight(ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        linearLayout.addView(tallBtn);
+        //넘겨줄 데이터 변수로 저장
+        String[] alltag = {"전체 원재료"};
+        //버튼 클릭 이벤트
+        tallBtn.setOnClickListener(v -> {
+            //원재료명, 태그, 설명을 showInfo 액티비티로 넘겨줌
+            Intent intent = new Intent(getApplicationContext(), ShowInfo.class);
+            intent.putExtra("tag", alltag);
+            intent.putExtra("rms",rms);
+            startActivity(intent);
+        });
+
+        // 각 테그에 대해서 버튼 생성
         for (RawMaterials rm : rms) {
-            //버튼 생성
             String[] splitarr = rm.getTags().split(" ");
             for (int i=0; i<splitarr.length;i++){
                 if(splitarr[i]=="" || splitarr[i]==null) continue;
