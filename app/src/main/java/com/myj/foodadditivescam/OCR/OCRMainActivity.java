d44e0cfed1049e21ba7321941659b177d7063464
지인 <sonia9.shin@gmail.com>
2021-07-15 14:38:42 +0900

    Revert "망했음"
    This reverts commit 4e1407ee5379199d9484c3c7a4030e0d8107ab07.

--- app/src/main/java/com/myj/foodadditivescam/OCR/OCRMainActivity.java
+++ app/src/main/java/com/myj/foodadditivescam/OCR/OCRMainActivity.java
@@ -33,10 +33,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -61,10 +59,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.myj.foodadditivescam.RawMaterials;
-import com.myj.foodadditivescam.SoftKeyboardDectectorView;
 import com.myj.foodadditivescam.result.ShowResult;
 import com.myj.foodadditivescam.search.SearchAPI;
 import com.myj.foodadditivescam.search.Symspell;
